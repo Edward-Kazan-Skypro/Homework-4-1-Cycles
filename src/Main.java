4ff17f6425dbc2baa4fdaebf695d94b8e84a6b7a
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-10-12 12:58:32 +0300

    Прошу проверить домашнее задание - часть 4 - 1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Решение задачи 1");
+        int i;
+        for (i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //Задание 2
+        System.out.println("Решение задачи 2");
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //Задание 3
+        System.out.println("Решение задачи 3");
+        for (i = 0; i < 17; i = i + 2) {
+           System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //Задание 4
+        System.out.println("Решение задачи 4");
+        for (i = 10; i > -11; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
