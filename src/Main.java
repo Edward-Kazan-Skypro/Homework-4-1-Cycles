536d1c809622ee93ebd4b642c049cf41815e0bfd
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-10-13 15:22:50 +0300

    Прошу проверить домашнее задание - часть 4 - 1

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,57 @@ public class Main {
         for (i = 10; i > -11; i--) {
             System.out.print(i + " ");
         }
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Решение задачи - високосный год");
+        for (i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        System.out.println();
+
+        //Задание 2
+        System.out.println("Решение задачи:");
+        for (i = 7; i <= 98; i = i + 7) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //Задание 3
+        System.out.println("Решение задачи:");
+        for (i = 1; i <= 512; i = i * 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Решение задачи:");
+        int total = 0;
+        int cash = 29000;
+        for (i = 1; i <=12; i++) {
+            total = total + cash;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        System.out.println();
+
+        System.out.println("Решение задачи:");
+        total = 0;
+        cash = 29000;
+
+        for (i = 1; i <=12; i++) {
+            total = total + total/100;
+            total = total + cash;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+
+
+
     }
 }
